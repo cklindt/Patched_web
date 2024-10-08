f27588b9151d1386375cf78007d74fa4eb746b1c
Jonny Wells <jonathanwells23@gmail.com>
2024-10-08 13:44:59 -0500

    changed forgot password to select user instead of type it

--- app/routes/forgot_password.py
+++ app/routes/forgot_password.py
@@ -1,19 +1,29 @@
 from flask import Blueprint, request, redirect, render_template, url_for
 
 from app.database import get_db_connection
-from .session import get_user
 
 forgot_pass_bp = Blueprint('forgot_password', __name__)
 
 @forgot_pass_bp.route('/forgot_password', methods=['GET', 'POST'])
 def forgot_password():
+    try:
+        with get_db_connection() as conn:
+            with conn.cursor() as cur:
+                # Get usernames
+                cur.execute("SELECT username FROM users ORDER BY user_id ASC")
+                users = cur.fetchall()
+
+                users = [user[0] for user in users]
+    except Exception as e:
+        print(e)
+
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
         if not username or not password:
             return render_template('forgot_password.html', error='Missing username or password')
-        
+
         try:
             with get_db_connection() as conn:
                 with conn.cursor() as cur:
@@ -35,9 +45,9 @@ def forgot_password():
                         )
                         conn.commit()
 
-            return render_template('forgot_password.html', success='Password reset successfully.')
+            return render_template('forgot_password.html', success='Password reset successfully.', users=users)    
 
         except Exception as e:
             return render_template('forgot_password.html', error=e)
-
-    return render_template('forgot_password.html')
+        
+    return render_template('forgot_password.html', users=users)
